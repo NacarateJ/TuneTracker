72f1706c876a77de8b7611dce4e0b8766ad94089
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-21 22:35:09 -0700

    Request data from API
    Created a private property to store the URL, injected HttpClient as http, created method to send the GET request to the API.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
+  private iTunesApiUrl =
+    'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
 
-  constructor() {}
+  // Dependency injection - makes the  HttpClient available for making HTTP requests
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    return this.http
+      .get(this.iTunesApiUrl)
+      .pipe(tap((response) => console.log('API Response SERVICE', response)));
+  }
 }
