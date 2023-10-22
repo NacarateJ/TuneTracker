f3fb1405a2e8b0abac145569f83c355a591840af
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-21 22:56:26 -0700

    Use DataService to fetch data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  fetchedData$: Observable<any>;
 
+  constructor(private dataService: DataService) {
+    this.fetchedData$ = this.dataService.getData().pipe(
+      tap((data) => {
+        console.log('API Response HOME', data);
+      })
+    );
+  }
 }
