0e787d44cd42e188f28119a13a2c3b4d9a4173d4
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 03:16:10 -0700

    Add logic to custom pipe
    The pipe filters a list of items based on a search string, included a utility function getPropertyValue for accessing nested properties.

--- src/app/shared/filter.pipe.ts
+++ src/app/shared/filter.pipe.ts
@@ -1,12 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
+  transform(value: any[], filterString: string, properties: string[]): any[] {
+    if (
+      !value ||
+      filterString === '' ||
+      !properties ||
+      properties.length === 0
+    ) {
+      return value;
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return value.filter((item) => {
+      const itemValues = properties.map((prop) =>
+        this.getPropertyValue(item, prop)
+      );
+      const lowercaseFilter = filterString.toLowerCase();
+
+      return itemValues.some((value) => {
+        if (typeof value === 'string') {
+          return value.toLowerCase().includes(lowercaseFilter);
+        }
+        return false;
+      });
+    });
   }
 
+  private getPropertyValue(item: any, propName: string): any {
+    const props = propName.split('.');
+    return props.reduce((obj, prop) => (obj && obj[prop]) || null, item);
+  }
 }
