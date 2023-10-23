840fd6601a65be1f35a98c0a87d453647dff0d6c
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 16:02:16 -0700

    Inject HomeStateService and update showHome state

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { HomeStateService } from 'src/app/services/home-state.service';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent {
+  constructor(private HomeStateService: HomeStateService) {}
 
+  showHome() {
+    this.HomeStateService.setShowHome(true);
+  }
 }
