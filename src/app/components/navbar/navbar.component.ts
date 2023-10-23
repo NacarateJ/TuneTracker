ddc163df725b1b10632825b277550d1b62b19bfc
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 03:55:17 -0700

    Create searchTerm property and search method

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  public searchTerm: string = '';
 
+  search(event: any) {
+    this.searchTerm = (event.target as HTMLInputElement).value;
+    console.log(this.searchTerm);
+  }
 }
