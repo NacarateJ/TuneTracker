67faaca90dfe2a3f1b9bb3250af1363bea03acd8
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 16:00:17 -0700

    Add logic to manage visibility state of home component
    Used a BehaviorSubject to provide an observable (showHome$) that components can subscribe to in order to monitor changes in the showHome state. Created setShowHome function that allows other parts of the application to update the showHome state.

--- src/app/services/home-state.service.ts
+++ src/app/services/home-state.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeStateService {
+  private showHomeComponent = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  showHome$: Observable<boolean> = this.showHomeComponent.asObservable();
+
+  setShowHome(value: boolean) {
+    this.showHomeComponent.next(value);
+  }
 }
