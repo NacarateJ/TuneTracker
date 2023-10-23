8d4b225582783cdf3c173c8ba5e6d03ae06be969
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 16:06:13 -0700

    Subscribe to showHome observable
    Injected HomeStateService and subscribed to showHome observable from HomeStateService to react to changes in the showHome state.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { HomeStateService } from 'src/app/services/home-state.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -7,14 +8,18 @@ import { Observable } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   fetchedData$: Observable<any>;
   searchKey: string = '';
   noResultsMessage: string =
     'Sorry, there are no albums that match your search';
+  showHome: boolean = false;
 
   // Dependency injection - allows the component to use the service to fetch data
-  constructor(private dataService: DataService) {
+  constructor(
+    private dataService: DataService,
+    private homeStateService: HomeStateService
+  ) {
     this.fetchedData$ = this.dataService.getData();
   }
 
@@ -23,6 +28,10 @@ export class HomeComponent {
   }
 
   ngOnInit() {
+    this.homeStateService.showHome$.subscribe((showHome) => {
+      this.showHome = showHome;
+    });
+
     this.dataService.search.subscribe((val: any) => {
       this.searchKey = val;
     });
