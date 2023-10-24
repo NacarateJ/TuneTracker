a978272f25d9ed671e48b71466b208cc592869e7
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-24 00:29:28 -0700

    Update tests configuration
    Added missing dependencies and components and updated title test to check for the title in an h1 tag.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule, FormsModule],
+      declarations: [
+        AppComponent,
+        NavbarComponent,
+        HeroComponent,
+        HomeComponent,
+      ],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,10 +32,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('TuneTracker');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TuneTracker app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'TuneTracker'
+    );
   });
 });
