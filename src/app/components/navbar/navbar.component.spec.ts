277eecaf0167b95ff19a312114e2814badeed49e
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 23:48:16 -0700

    Mock dataService
    Mocked dataService with a search object that has a next method, added formModule to imports in testBed.

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { NavbarComponent } from './navbar.component';
+
+import { DataService } from 'src/app/services/data.service';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
+  // Create a mock DataService
+  const dataServiceStub = {
+    search: { next: () => {} },
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
+      declarations: [NavbarComponent],
+      imports: [FormsModule],
+      providers: [{ provide: DataService, useValue: dataServiceStub }],
     });
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
