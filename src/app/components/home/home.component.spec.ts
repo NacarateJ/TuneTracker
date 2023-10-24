014053d3c2a87d118a05d381411d85aaf7127173
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 23:33:17 -0700

    Create mock implementations of dataService and homeStateService
    Added services to providers in testBed.

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { DataService } from 'src/app/services/data.service';
+import { HomeStateService } from 'src/app/services/home-state.service';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  // Create mock services
+  const dataServiceStub = {
+    getData: () => of([]),
+    search: of(''),
+  };
+
+  const homeStateServiceStub = {
+    showHome$: of(false),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      providers: [
+        { provide: DataService, useValue: dataServiceStub },
+        { provide: HomeStateService, useValue: homeStateServiceStub },
+      ],
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
