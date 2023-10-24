67a2617fde0c7d2db86d77ec12f464bcba072ddd
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-23 23:13:31 -0700

    Configute TestBed with necessary Providers

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -2,11 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('DataService', () => {
   let service: DataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [DataService],
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DataService);
   });
 
