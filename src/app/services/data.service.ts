5a580ee4c6a7d1bef308f097cd74afe30d004326
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-11-03 16:43:56 -0700

    Handle errors during HTTP requests
    Refactored getData to include error handling using catchError uperator, defined handleError method to log the error and rethrow it so that whoever subscribes to the getData observable can handle it .

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,13 +10,25 @@ export class DataService {
   private iTunesApiUrl =
     'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
 
-  public search = new BehaviorSubject<string>("");
+  public search = new BehaviorSubject<string>('');
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An unknown error has occurred!';
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred.
+      errorMessage = `An error occurred: ${error.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      errorMessage = `Server returned code: ${error.status}, error message is: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => errorMessage);
+  }
 
   // Dependency injection - makes the  HttpClient available for making HTTP requests
   constructor(private http: HttpClient) {}
 
   getData() {
-    return this.http
-      .get(this.iTunesApiUrl)
+    return this.http.get(this.iTunesApiUrl).pipe(catchError(this.handleError));
   }
 }
