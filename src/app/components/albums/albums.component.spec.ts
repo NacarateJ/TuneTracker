baab06a3c5fcd3782c824d808180c53a6abf6682
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-11-04 11:48:30 -0700

    Test error handling
    Added new test cases to verify the component's behavior when errors occur. To simulate errors in the observables, the throwError function is used to return an error when the observable is subscribed to.

--- src/app/components/albums/albums.component.spec.ts
+++ src/app/components/albums/albums.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AlbumsComponent } from './albums.component';
 import { DataService } from 'src/app/services/data.service';
 import { AlbumsStateService } from 'src/app/services/albums-state.service';
-import { of, Observable } from 'rxjs';
+import { of, Observable, throwError } from 'rxjs';
 
 describe('AlbumsComponent', () => {
   let component: AlbumsComponent;
@@ -47,5 +47,53 @@ describe('AlbumsComponent', () => {
       { showAlbums$: of(false) }
     );
     expect(component).toBeTruthy();
+  });
+
+  it('should handle errors when fetchedData$ fails', () => {
+    configureTestingModule(
+      {
+        getData: () => throwError(() => new Error('Error fetching data')),
+        search: of(''),
+      },
+      { showAlbums$: of(false) }
+    );
+
+    expect(component.hasError).toBeTrue();
+    expect(component.errorMessage).toBe(
+      'Failed to fetch albums. Please try again later.'
+    );
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      'Error fetching data: ',
+      jasmine.any(Error)
+    );
+  });
+
+  it('should handle errors in showAlbums$ subscription', () => {
+    configureTestingModule(
+      { getData: () => of([]), search: of('') },
+      { showAlbums$: throwError(() => new Error('Error with showAlbums$')) }
+    );
+
+    expect(component.hasError).toBeTrue();
+    expect(component.errorMessage).toBe(
+      'An error occurred while managing albums visibility.'
+    );
+    expect(consoleErrorSpy).toHaveBeenCalled();
+  });
+
+  it('should handle errors in search subscription', () => {
+    configureTestingModule(
+      {
+        getData: () => of([]),
+        search: throwError(() => new Error('Error with search')),
+      },
+      { showAlbums$: of(false) }
+    );
+
+    expect(component.hasError).toBeTrue();
+    expect(component.errorMessage).toBe(
+      'An error occurred while handling search functionality.'
+    );
+    expect(consoleErrorSpy).toHaveBeenCalled();
   });
 });
