f1b9876b14c0ca63ba7b8c6775053d0e6622b66c
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-11-07 17:02:16 -0800

    Test state management
    Added test to confirm default state of showAlbums$ as false and tested setShowAlbums method to ensure it emits true and false appropriately.

--- src/app/services/albums-state.service.spec.ts
+++ src/app/services/albums-state.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AlbumsStateService } from './albums-state.service';
 
 describe('AlbumsStateService', () => {
@@ -12,5 +11,29 @@ describe('AlbumsStateService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should default showAlbums$ to false', (done) => {
+    service.showAlbums$.subscribe((value) => {
+      expect(value).toBeFalse();
+      done();
+    });
+  });
+
+  it('should emit true when setShowAlbums is called with true', (done) => {
+    service.setShowAlbums(true);
+    service.showAlbums$.subscribe((value) => {
+      expect(value).toBeTrue();
+      done();
+    });
+  });
+
+  it('should emit false when setShowAlbums is called with false', (done) => {
+    // This is to change the state if it's already true.
+    service.setShowAlbums(false);
+    service.showAlbums$.subscribe((value) => {
+      expect(value).toBeFalse();
+      done();
+    });
   });
 });
